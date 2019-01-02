01f0d325a60aaf4347622530bb8a1d0e02481da7
Michel Weststrate <mweststrate@gmail.com>
2019-01-02 20:15:28 +0100

    Project setup / implementation

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function st8() {
+    switch(arguments.length) {
+        case 0: return this[0]
+        case 1: this[1](arguments[0])
+        default: throw new Error("Expected 0 or 1 arguments")
+    }
+}
+
+export function useSt8<T>(initial: (() => T) | T): {
+    (): T
+    (newValue: T | ((current: T) => T)): void
+} {
+    return st8.bind(useState(initial))
+}
