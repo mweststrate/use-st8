0a562b3c905413710cd6c0f50d95372676a64be1
r1nzai <60702701+r1nzai@users.noreply.github.com>
2022-07-15 03:22:26 +0530

    unexpected

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import { useMemo, useState } from "react";
 
 export type St8<V> = {
-  (): V;
-  (newValue: V | ((current: V) => V)): void;
+	(): V;
+	(newValue: V | ((current: V) => V)): void;
 };
 
 export function useSt8<V>(initialValue: (() => V) | V): St8<V> {
-  const [state, setState] = useState(initialValue);
-  return useMemo(() => {
-    return function st8() {
-      switch (arguments.length) {
-        case 0:
-          return state;
-        case 1:
-          return void setState(arguments[0]);
-        default:
-          throw new Error("Expected 0 or 1 arguments");
-      }
-    } as St8<V>;
-  }, [state]);
+	const [state, setState] = useState(initialValue);
+	return useMemo(() => {
+		return function st8() {
+			switch (arguments.length) {
+				case 0:
+					return state;
+				case 1:
+					return void setState(arguments[0]);
+				default:
+					throw new Error("Expectd 0 or 1 arguments");
+			}
+		} as St8<V>;
+	}, [state]);
 }
